4e689c827ebf3be6936c50e4593fbed1239b2986
muxiongguo <1721030720@qq.com>
2023-02-27 00:09:40 +0800

    🌀 数据结构 栈，go语言切片的理解

--- src/sword1/s09.go
+++ src/sword1/s09.go
@@ -1,18 +1,30 @@
 package sword1
 
 type CQueue struct {
+	inStack, outStack []int
 }
 
 func Constructor() CQueue {
-
+	return CQueue{}
 }
 
 func (this *CQueue) AppendTail(value int) {
-
+	this.inStack = append(this.inStack, value)
 }
 
 func (this *CQueue) DeleteHead() int {
-
+	if len(this.outStack) == 0 {
+		if len(this.inStack) == 0 {
+			return -1
+		}
+		for i := len(this.inStack) - 1; i >= 0; i-- {
+			this.outStack = append(this.outStack, this.inStack[i])
+		}
+		this.inStack = this.inStack[:0]
+	}
+	value := this.outStack[len(this.outStack)-1]
+	this.outStack = this.outStack[:len(this.outStack)-1]
+	return value
 }
 
 /**
