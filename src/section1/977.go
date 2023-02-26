950081ab97a74b6a6645ba96675ed96ecec2bc40
muxiongguo <1721030720@qq.com>
2023-02-26 13:12:48 +0800

    🥢 双指针，两头填充

--- src/section1/977.go
+++ src/section1/977.go
@@ -11,19 +11,30 @@ func sortedSquares(nums []int) []int {
 	left, right := midIndex-1, midIndex+1
 	ans[0] = nums[midIndex] * nums[midIndex]
 	for i := 1; i < len(nums); i++ {
-		if left < 0 {
+		if left < 0 || (right < len(nums) && nums[left]*nums[left] > nums[right]*nums[right]) {
 			ans[i] = nums[right] * nums[right]
 			right++
-		} else if right >= len(nums) {
-			ans[i] = nums[left] * nums[left]
-			left--
-		} else if nums[left]*nums[left] < nums[right]*nums[right] {
-			ans[i] = nums[left] * nums[left]
-			left--
 		} else {
-			ans[i] = nums[right] * nums[right]
-			right++
+			ans[i] = nums[left] * nums[left]
+			left--
 		}
+	}
+	return ans
+}
+
+// 官方 充分利用双指针，这次从两边开始，不断向中间，太聪明了吧，细节是index倒着
+func sortedSquares2(nums []int) []int {
+	ans := make([]int, len(nums))
+	left, right, i := 0, len(nums)-1, len(nums)-1
+	for left <= right {
+		if nums[left]*nums[left] <= nums[right]*nums[right] {
+			ans[i] = nums[right] * nums[right]
+			right--
+		} else {
+			ans[i] = nums[left] * nums[left]
+			left++
+		}
+		i--
 	}
 	return ans
 }
