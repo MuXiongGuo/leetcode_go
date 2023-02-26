66ac11b6e35f4588d03704b148f701eb9bc4eb06
muxiongguo <muxiongguo@gmail.com>
2023-02-26 19:33:31 +0800

    🥢 双指针，滑动窗口，前缀和+二分(lower_bound)

--- src/section1/209.go
+++ src/section1/209.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"sort"
 )
 
 // O(n) 滑动窗口
@@ -28,7 +29,7 @@ func minSubArrayLen(target int, nums []int) int {
 // O(n*lgn) 前缀和+二分查找
 // 本质上是对暴力枚举的优化
 func minSubArrayLen2(target int, nums []int) int {
-	n, ans := len(nums), math.MinInt32
+	n, ans := len(nums), math.MaxInt32
 	preSums := make([]int, n)
 	for i, _ := range nums {
 		if i == 0 {
@@ -37,5 +38,48 @@ func minSubArrayLen2(target int, nums []int) int {
 			preSums[i] = preSums[i-1] + nums[i]
 		}
 	}
+	for i := 0; i <= n-1; i++ {
+		if i == 0 {
+			bound := sort.SearchInts(preSums, target)
+			if bound == n {
+				continue
+			}
+			ans = min(ans, bound+1)
+		} else {
+			bound := sort.SearchInts(preSums, target+preSums[i-1])
+			if bound == n {
+				continue
+			}
+			ans = min(ans, bound-i+1)
+		}
+	}
+	if ans == math.MaxInt32 {
+		return 0
+	}
+	return ans
+}
 
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func lowerBound(nums []int, target int) int {
+	left, right, ans := 0, len(nums)-1, len(nums)
+	for left <= right {
+		mid := (right + left) / 2
+		if nums[mid] >= target {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return ans
+}
+
+func main() {
+	println(minSubArrayLen2(7, []int{2, 3, 1, 2, 4, 3}))
 }
