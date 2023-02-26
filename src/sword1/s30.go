778d09f0e5b216048db7f1c54b6c5de871f808b9
muxiongguo <1721030720@qq.com>
2023-02-27 00:22:31 +0800

    TODO

--- src/sword1/s30.go
+++ src/sword1/s30.go
@@ -1,1 +1,16 @@
-package sword1
+package main
+
+import "fmt"
+
+type MinStack struct {
+	data []int
+}
+
+func main() {
+	d := MinStack{}
+	c := make([]int, 0)
+	c = append(c, 8)
+	d.data = append(d.data, 55)
+	fmt.Println(d)
+	fmt.Println(c)
+}
