aca4532bba1769e44bd2e10d1fb1358588782854
muxiongguo <muxiongguo@gmail.com>
2023-02-27 20:04:08 +0800

    🍆 链表

--- src/sword1/s06.go
+++ src/sword1/s06.go
@@ -5,12 +5,14 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// 1. 递归做法, 很经典的用递归来删除 倒序
+// 重点是学学切片的底层 以及切片的指针
 func helper(head *ListNode, ans *[]int) {
 	if head == nil {
 		return
 	}
 	helper(head.Next, ans)
-	ans = append(ans, head.Val)
+	*ans = append(*ans, head.Val)
 }
 func reversePrint(head *ListNode) []int {
 	var ans []int
