b902b3f97840befc6b2fb9f6d712812a70a578ed
muxiongguo <muxiongguo@gmail.com>
2023-02-28 20:12:08 +0800

    🍌 字符串

---
+++ src/sword1/s58.go
@@ -0,0 +1,24 @@
+package main
+
+func reverseLeftWords(s string, n int) string {
+	s1 := []byte(s)
+	reverseString(s1[:n])
+	reverseString(s1[n:])
+	reverseString(s1)
+	return string(s1)
+}
+
+func reverseString(s []byte) {
+	low, high := 0, len(s)-1
+	for low < high {
+		s[low], s[high] = s[high], s[low]
+		low++
+		high--
+	}
+	return s
+}
+func main() {
+	s := "abcdefg"
+	n := 2
+	println(reverseLeftWords(s, n))
+}
