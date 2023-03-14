bce48c723dd1e1f68e685f0b8dfaf6cd0d42f8bb
muxiongguo <muxiongguo@gmail.com>
2023-03-14 21:39:39 +0800

    🐝 打草练习

--- src/section1/test.go
+++ src/section1/test.go
@@ -1,7 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
-	a := 13
-	b := "dawdw"
-	println(a == b[0])
+	// 类型相同才能比较
+	//a := 13
+	//b := "dawdw"
+	//println(a == b[0])
+	// 匿名函数即函数字面量
+	//a := 33
+	//f1 := func(a, b int) int {
+	//	fmt.Println(a + b)
+	//	return a + b
+	//}(3, a)
+	//f2 := func(a, b int) int {
+	//	fmt.Println(a + b)
+	//	return a + b
+	//}
+	//fmt.Printf("%T, %T\n", f1, f2)
+	//a += 333
+	//b := f1
+	//fmt.Println(b)
+	//a += 111
+	q := []int{9}
+	q = q[:0]
+	fmt.Println(q)
 }
