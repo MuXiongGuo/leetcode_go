8b1f73fb291c0453ae7f381b8a391bfc3e69f660
muxiongguo <muxiongguo@gmail.com>
2023-03-15 20:20:51 +0800

    TODO

--- src/everyday/1615.go
+++ src/everyday/1615.go
@@ -9,7 +9,7 @@ type pair struct {
 
 type pairSlice []pair
 
-func (p []pair) Less(i, j int) bool {
+func (p pairSlice) Less(i, j int) bool {
 	return p[i].count > p[j].count
 }
 func (p pairSlice) Len() int {
@@ -19,13 +19,58 @@ func (p pairSlice) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
 
-//	func maximalNetworkRank(n int, roads [][]int) int {
-//		adList := make([][]int, n)
-//		adCount := make([]int, n)
-//
-// }
+// 两种方法实现排序pair问题
+func maximalNetworkRank(n int, roads [][]int) int {
+	adList := make([][]int, n)
+	for i := range adList {
+		adList[i] = make([]int, n)
+	}
+	pCount := make([]pair, n)
+	for i := range pCount {
+		pCount[i].index = i
+	}
+	for i := 0; i < len(roads); i++ {
+		adList[roads[i][0]][roads[i][1]]++
+		adList[roads[i][1]][roads[i][0]]++
+		pCount[roads[i][0]].count++
+		pCount[roads[i][1]].count++
+	}
+	sort.Slice(pCount, func(i, j int) bool {
+		return pCount[i].count > pCount[j].count
+	})
+	max1, max2 := pCount[0].count, pCount[1].count
+	index1 := pCount[0].index
+	// 分类讨论
+	if max1 != max2 {
+		for i := 1; i < n; i++ {
+			if pCount[i].count == max2 && adList[pCount[i].index][index1] == 0 {
+				return max1 + max2
+			}
+		}
+		return max1 + max2 - 1
+	} else {
+		index2 := 1
+		for i := 1; i < n; i++ {
+			if pCount[i].count == max1 {
+				index2 = i
+			}
+		}
+		for i := 0; i <= index2; i++ {
+			for j := i + 1; j <= index2; j++ {
+				if adList[pCount[i].index][pCount[j].index] == 0 {
+					return max1 + max2
+				}
+			}
+		}
+		return max1 + max2 - 1
+	}
+}
 func main() {
-	a := pairSlice{{1, 2}, {2, 3}, {3, 4}}
-	sort.Sort(a)
-	println(a)
+	//a := pairSlice{{1, 2}, {2, 3}, {3, 4}}
+	////sort.Sort(a)
+	//sort.Slice(a, func(i, j int) bool { return a[i].count > a[j].count })
+	//println(a)
+
+	roads := [][]int{{0, 1}, {0, 3}, {1, 2}, {1, 3}}
+	maximalNetworkRank(4, roads)
 }
