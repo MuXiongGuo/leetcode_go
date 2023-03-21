226029df3f02bbe18954ad1a0b080feaceb4374a
muxiongguo <muxiongguo@gmail.com>
2023-03-21 16:26:23 +0800

    🍓🌷 哈希+前缀和 转换思路 以及统计问题

--- src/everyday/2488.go
+++ src/everyday/2488.go
@@ -1,14 +1,36 @@
 package main
 
-// 1.前缀和统计
+// 1.哈希前缀和统计
 func countSubarrays(nums []int, k int) int {
-	var mid int
-	left, right := make([]int), []int{}
-	for i := range nums {
-		if nums[i] == k {
-			mid = i
+	mLeft, s, ans := map[int]int{}, 0, 0
+	var start int
+	for i, v := range nums {
+		if v == k {
+			start = i
 			break
 		}
 	}
-
+	for i := start; i >= 0; i-- {
+		if nums[i] > k {
+			s += 1
+		} else if nums[i] < k {
+			s -= 1
+		}
+		mLeft[s]++
+	}
+	s = 0
+	ans += mLeft[0] + mLeft[1]
+	for i := start + 1; i < len(nums); i++ {
+		if nums[i] > k {
+			s += 1
+		} else if nums[i] < k {
+			s -= 1
+		}
+		ans += mLeft[-s] + mLeft[-s+1]
+	}
+	return ans
+}
+func main() {
+	//[3,2,1,4,5], k = 4
+	countSubarrays([]int{3, 2, 1, 4, 5}, 4)
 }
