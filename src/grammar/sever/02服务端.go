019af80cdc68dce5cf011d59581411ac114216ad
muxiongguo <muxiongguo@gmail.com>
2023-04-05 22:32:30 +0800

    🐝 语法学习

--- src/grammar/sever/02服务端.go
+++ src/grammar/sever/02服务端.go
@@ -3,10 +3,27 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func HandleConn(conn net.Conn) {
+	defer conn.Close()
 
+	addr := conn.RemoteAddr().String()
+	buf := make([]byte, 2048)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("[%s]: %s\n", addr, string(buf[:n]))
+		if string(buf[:n-1]) == "exit" {
+			fmt.Println(addr, " exit")
+		}
+		// 转换成大写进行输出
+		conn.Write([]byte(strings.ToUpper(string(buf[:n]))))
+	}
 }
 func main() {
 	// 监听
@@ -29,6 +46,6 @@ func main() {
 			return
 		}
 		// 处理用户请求 每来一个用户就新建一个协程
-		HandleConn()
+		go HandleConn(conn)
 	}
 }
