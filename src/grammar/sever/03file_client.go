ca3d0a0827cba10d421a1dae45b7311d4506bc4a
muxiongguo <muxiongguo@gmail.com>
2023-04-06 11:37:21 +0800

    🐝 语法学习

--- src/grammar/sever/03file_client.go
+++ src/grammar/sever/03file_client.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	list := os.Args
+	if len(list) != 2 {
+		fmt.Println("useage: xxx file")
+		return
+	}
+
+	fileName := list[1]
+	info, err := os.Stat(fileName) // 获取文件属性
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("file name is ", info.Name())
+	fmt.Println("file size is ", info.Size())
+}
