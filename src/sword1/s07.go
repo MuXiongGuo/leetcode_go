4d4ee7d8834eccc66267462c4799eff65eef1b99
muxiongguo <muxiongguo@gmail.com>
2023-05-20 13:01:53 +0800

    🍄 重建二叉树 1.slice可以完美替代队列与栈, slice头部尾部切片时都支持溢出1个单位 2.官方利用返回值, 而我是使用不带返回值, 官方更好值得学习逻辑

--- src/sword1/s07.go
+++ src/sword1/s07.go
@@ -1,17 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-//	func buildTree(preorder []int, inorder []int) *TreeNode {
-//		var helper func(start, end int, p *TreeNode)
-//		helper = func(start, end int, p *TreeNode) {
-//			if start > end {
-//				return
-//			}
-//			p.Val = preorder[start]
-//
-//		}
+//	type TreeNode struct {
+//		Val   int
+//		Left  *TreeNode
+//		Right *TreeNode
 //	}
+//
+// 小心 指针的引用 已经不能用了 指针的指针才可以 但是可以巧妙地避免
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	find := func(s []int, x int) (ans int) {
+		for i, v := range s {
+			if x == v {
+				return i
+			}
+		}
+		return
+	}
+	var helper func(pre, in []int, p *TreeNode)
+	helper = func(pre, in []int, p *TreeNode) {
+		// 终止条件
+		if len(pre) == 0 {
+			return
+		}
+		p.Val = pre[0]
+		index := find(inorder, pre[0])
+		// 开辟新节点
+		if len(pre[1:index+1]) != 0 {
+			p.Left = &TreeNode{}
+		}
+		if len(pre[index+1:]) != 0 {
+			p.Right = &TreeNode{}
+		}
+		helper(pre[1:index+1], in[:index], p.Left)   // 单个元素s[1:] 不报错
+		helper(pre[index+1:], in[index+1:], p.Right) // 长度3  s[3:] 也不报错 很有趣的性质
+	}
+	if len(preorder) == 0 {
+		return nil
+	}
+	p := &TreeNode{}
+	helper(preorder, inorder, p)
+	return p
+}
+
+// 官方!!! 思路是一样的 但是完美利用了返回值!!!
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{preorder[0], nil, nil}
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:i+1], inorder[:i])
+	root.Right = buildTree(preorder[i+1:], inorder[i+1:])
+	return root
+}
+
 func main() {
 	// 一个空的 cap 为 5 的 slice
 	queue := make([]int, 0, 5) // _ _ _ _ _
@@ -51,4 +103,13 @@ func main() {
 	queue = queue[:len(queue)-1] //  // [4 5 6] _
 	fmt.Printf("len(queue): %v, cap(queue): %v, queue: %v\n", len(queue), cap(queue), queue)
 
+	s1 := []int{1, 2, 3}
+	s2 := s1[1:]
+	var s3 []int
+	fmt.Println(s2)
+	fmt.Println(s3)
+	s4 := s1[3:]
+	//p := &TreeNode{}
+	//fmt.Println(p)
+	fmt.Println(s4)
 }
