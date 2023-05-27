66817e5fec8e443ebb58a90bc2202692768a5541
muxiongguo <muxiongguo@gmail.com>
2023-05-27 20:52:23 +0800

    🍆 Ⅱ: 模板 1.dummyNode 2.p0为cur之前的核心指针, pre cur nxt用来翻转, 利用计数来反转,而不是指针判断!!!

--- src/section1/92.go
+++ src/section1/92.go
@@ -6,34 +6,57 @@ type ListNode struct {
 }
 
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	if left == right {
+		return head
+	}
 	l, r := head, head
-	var pre, next *ListNode
+	var pre, cur *ListNode
+	cur = r.Next
 	for i := 1; i < left; i++ {
 		pre = l
 		l = l.Next
 	}
 	for i := 1; i < right; i++ {
 		r = r.Next
-		next = r.Next
+		cur = r.Next
 	}
 	if pre != nil {
 		pre.Next = r
 	}
 
-	cur, p := l, next
-	var n *ListNode
-	end := r.Next
-	for cur != end {
-		n = cur.Next
-		cur.Next = p
-		p = cur
-		cur = n
+	var pPre, pNext, pCur *ListNode
+	pCur, pPre = l, cur
+	for pCur != cur {
+		pNext = pCur.Next
+		pCur.Next = pPre
+		pPre = pCur
+		pCur = pNext
 	}
 	if pre == nil {
 		return r
 	}
-	return pre
+	return head
 }
+
+// 使用dummyNode避免特殊情况
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	dummyNode := &ListNode{Next: head}
+	p0 := dummyNode
+	for i := 0; i < left-1; i++ {
+		p0 = p0.Next
+	}
+	var cur, pre *ListNode = p0.Next, nil
+	for i := 0; i <= right-left; i++ {
+		nxt := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = nxt
+	}
+	p0.Next.Next = cur
+	p0.Next = pre
+	return dummyNode.Next
+}
+
 func main() {
 	reverseBetween(&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, 2, 4)
 }
