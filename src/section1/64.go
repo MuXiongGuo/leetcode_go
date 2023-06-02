73b01a0953839cb20b951a414c324678a4e07078
muxiongguo <muxiongguo@gmail.com>
2023-06-02 11:55:33 +0800

    🌈 比较基础的dp 可以考虑用低维变量试试 或者 原数组上修改

--- src/section1/64.go
+++ src/section1/64.go
@@ -22,3 +22,28 @@ func dfs(grid [][]int, x, y, z int) int {
 func minPathSum(grid [][]int) int {
 	return dfs(grid, 0, 0, 0)
 }
+
+// 2. dp
+func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if i == 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			} else if j == 0 {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			} else {
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
