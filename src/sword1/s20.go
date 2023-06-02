f5a538587c1f728375f629c555f90b23cd303d2c
muxiongguo <muxiongguo@gmail.com>
2023-06-02 11:26:32 +0800

    🍒 有限状态机

--- src/sword1/s20.go
+++ src/sword1/s20.go
@@ -4,98 +4,100 @@ type State int
 type CharType int
 
 const (
-	STATE_INITIAL State = iota
-	STATE_INT_SIGN
+	START_SPACE State = iota
+	STATE_SIGN
+	STATE_POINT_WITHOUT_NUMBER
 	STATE_INTEGER
 	STATE_POINT
-	STATE_POINT_WITHOUT_INT
 	STATE_FRACTION
 	STATE_EXP
 	STATE_EXP_SIGN
-	STATE_EXP_NUMBER
-	STATE_END
+	STATE_EXP_INTEGER
+	STATE_END_SPACE
 )
 
 const (
-	CHAR_NUMBER CharType = iota
-	CHAR_EXP
-	CHAR_POINT
-	CHAR_SIGN
-	CHAR_SPACE
-	CHAR_ILLEGAL
+	SPACE CharType = iota
+	POINT
+	NUMBER
+	EXP
+	SIGN
+	ILLEGAL
 )
 
 func toCharType(ch byte) CharType {
 	switch ch {
-	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-		return CHAR_NUMBER
-	case 'e', 'E':
-		return CHAR_EXP
-	case '.':
-		return CHAR_POINT
-	case '+', '-':
-		return CHAR_SIGN
 	case ' ':
-		return CHAR_SPACE
+		return SPACE
+	case '.':
+		return POINT
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return NUMBER
+	case 'e', 'E':
+		return EXP
+	case '+', '-':
+		return SIGN
 	default:
-		return CHAR_ILLEGAL
+		return ILLEGAL
 	}
 }
 
+// 映射有 map[state, char]->state 和 map[state]->map[char]->state  后者更简洁好用
 func isNumber(s string) bool {
-	transfer := map[State]map[CharType]State{
-		STATE_INITIAL: map[CharType]State{
-			CHAR_SPACE:  STATE_INITIAL,
-			CHAR_NUMBER: STATE_INTEGER,
-			CHAR_POINT:  STATE_POINT_WITHOUT_INT,
-			CHAR_SIGN:   STATE_INT_SIGN,
+	m := map[State]map[CharType]State{
+		START_SPACE: map[CharType]State{
+			SPACE:  START_SPACE,
+			NUMBER: STATE_INTEGER,
+			POINT:  STATE_POINT_WITHOUT_NUMBER,
+			SIGN:   STATE_SIGN,
 		},
-		STATE_INT_SIGN: map[CharType]State{
-			CHAR_NUMBER: STATE_INTEGER,
-			CHAR_POINT:  STATE_POINT_WITHOUT_INT,
+		STATE_SIGN: {
+			POINT:  STATE_POINT_WITHOUT_NUMBER,
+			NUMBER: STATE_INTEGER,
 		},
-		STATE_INTEGER: map[CharType]State{
-			CHAR_NUMBER: STATE_INTEGER,
-			CHAR_EXP:    STATE_EXP,
-			CHAR_POINT:  STATE_POINT,
-			CHAR_SPACE:  STATE_END,
+		STATE_INTEGER: {
+			NUMBER: STATE_INTEGER,
+			SPACE:  STATE_END_SPACE,
+			POINT:  STATE_POINT,
+			EXP:    STATE_EXP,
 		},
-		STATE_POINT: map[CharType]State{
-			CHAR_NUMBER: STATE_FRACTION,
-			CHAR_EXP:    STATE_EXP,
-			CHAR_SPACE:  STATE_END,
+		STATE_POINT_WITHOUT_NUMBER: {
+			NUMBER: STATE_FRACTION,
 		},
-		STATE_POINT_WITHOUT_INT: map[CharType]State{
-			CHAR_NUMBER: STATE_FRACTION,
+		STATE_POINT: {
+			SPACE:  STATE_END_SPACE,
+			EXP:    STATE_EXP,
+			NUMBER: STATE_FRACTION,
 		},
-		STATE_FRACTION: map[CharType]State{
-			CHAR_NUMBER: STATE_FRACTION,
-			CHAR_EXP:    STATE_EXP,
-			CHAR_SPACE:  STATE_END,
+		STATE_FRACTION: {
+			NUMBER: STATE_FRACTION,
+			SPACE:  STATE_END_SPACE,
+			EXP:    STATE_EXP,
 		},
-		STATE_EXP: map[CharType]State{
-			CHAR_NUMBER: STATE_EXP_NUMBER,
-			CHAR_SIGN:   STATE_EXP_SIGN,
+		STATE_EXP: {
+			SIGN:   STATE_EXP_SIGN,
+			NUMBER: STATE_EXP_INTEGER,
 		},
-		STATE_EXP_SIGN: map[CharType]State{
-			CHAR_NUMBER: STATE_EXP_NUMBER,
+		STATE_EXP_SIGN: {
+			NUMBER: STATE_EXP_INTEGER,
 		},
-		STATE_EXP_NUMBER: map[CharType]State{
-			CHAR_NUMBER: STATE_EXP_NUMBER,
-			CHAR_SPACE:  STATE_END,
+		STATE_EXP_INTEGER: {
+			NUMBER: STATE_EXP_INTEGER,
+			SPACE:  STATE_END_SPACE,
 		},
-		STATE_END: map[CharType]State{
-			CHAR_SPACE: STATE_END,
+		STATE_END_SPACE: {
+			SPACE: STATE_END_SPACE,
 		},
 	}
-	state := STATE_INITIAL
+
+	state := START_SPACE
 	for i := 0; i < len(s); i++ {
-		typ := toCharType(s[i])
-		if _, ok := transfer[state][typ]; !ok {
+		v := toCharType(s[i])
+		if _, ok := m[state][v]; !ok {
 			return false
 		} else {
-			state = transfer[state][typ]
+			state = m[state][v]
 		}
 	}
-	return state == STATE_INTEGER || state == STATE_POINT || state == STATE_FRACTION || state == STATE_EXP_NUMBER || state == STATE_END
+	return state == STATE_END_SPACE || state == STATE_EXP_INTEGER || state == STATE_FRACTION || state == STATE_POINT || state == STATE_INTEGER
 }
