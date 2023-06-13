6ed0f562d3d88a1407b4b0b18236a2dd0f0bf79d
muxiongguo <muxiongguo@gmail.com>
2023-06-13 09:38:42 +0800

    🥝 滑动窗口 r-l 就是子数组长度 细节!

---
+++ src/section1/713.go
@@ -0,0 +1,34 @@
+package main
+
+// 注意left可能会超过right的细节即可
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	left, ans, cur, n, cnt := 0, 0, 1, len(nums), 0
+	for right := 0; right < n; right++ {
+		cur *= nums[right]
+		cnt++
+		for cur >= k && left <= right {
+			cur /= nums[left]
+			left++
+			cnt--
+		}
+		ans += cur
+	}
+	return ans
+}
+
+// 灵神 完美！！！  right-left 已经是长度了！！！
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	if k <= 1 {
+		return 0
+	}
+	left, ans, cur, n := 0, 0, 1, len(nums)
+	for right := 0; right < n; right++ {
+		cur *= nums[right]
+		for cur >= k {
+			cur /= nums[left]
+			left++
+		}
+		ans += right - left + 1
+	}
+	return ans
+}
