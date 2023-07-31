b7b6b72a2152288864d93dabe93401e5bef8269a
muxiongguo <1721030720@qq.com>
2023-07-31 23:26:01 +0800

    🥢 二分 螺旋，每次分两段，在另一段中不断继续二分查找

--- src/section1/33.go
+++ src/section1/33.go
@@ -38,6 +38,44 @@ func findMin(nums []int) int {
 	return low
 }
 
+// 官方
+// 将数组一分为二，其中一定有一个是有序的，另一个可能是有序，也能是部分有序。
+// 此时有序部分用二分法查找。无序部分再一分为二，其中一个一定有序，另一个可能有序，可能无序。就这样循环.
+func search(nums []int, target int) int {
+	n := len(nums)
+	if n == 0 {
+		return -1
+	}
+	if n == 1 {
+		if nums[0] == target {
+			return 0
+		}
+		return -1
+	}
+
+	l, r := 0, n-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[0] <= nums[mid] { // mid 在有序的那一部分里面 [l, mid]有序
+			if nums[0] <= target && target < nums[mid] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		} else { // mid 在矮小的部分 [l, mid]无序
+			if nums[mid] < target && target <= nums[r] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
 // c++  很巧妙
 //class Solution {
 //public:
