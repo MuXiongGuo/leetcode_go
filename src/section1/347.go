3e41b6903fe29ececd22c331f858f1f5899191f4
muxiongguo <muxiongguo@gmail.com>
2023-08-08 16:30:12 +0800

    🍚🍀 优先队列堆 0. 找到前k大模板 带随机数版本 1.golang如何定制自己想要的堆 2.引用类型的底层共享 传递 指针的加入 3.引用类型:channel slice map interface string 4.切片append传递时 有可能直接偷跑 扩容后会丢失 可以用指针就没事了

--- src/section1/347.go
+++ src/section1/347.go
@@ -136,3 +136,44 @@ func qsort(values [][]int, start, end int, ret []int, retIndex, k int) {
 		}
 	}
 }
+
+// 自己手写一下 TopK模板
+func topKFrequent(nums []int, k int) []int {
+	m := map[int]int{}
+	for _, val := range nums {
+		m[val]++
+	}
+	var q [][]int
+	for k, v := range m {
+		q = append(q, []int{k, v})
+	}
+	var res []int
+	qSort(q, &res, 0, len(q)-1, k, 0)
+	return res
+}
+
+func qSort(nums [][]int, res *[]int, l, r, k, curResIdx int) {
+	rand.Seed(time.Now().UnixNano())
+	x := rand.Int()%(r-l+1) + l
+	nums[x], nums[r] = nums[r], nums[x]
+	i, j := l-1, l
+	for ; j < r; j++ {
+		if nums[j][1] > nums[r][1] {
+			i++
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+	i++
+	nums[i], nums[j] = nums[j], nums[i]
+	if k < i+1-l { // k的判断也要修改  之前第k个不用改太多
+		qSort(nums, res, l, i-1, k, curResIdx)
+	} else {
+		for _, el := range nums[l : i+1] {
+			*res = append(*res, el[0])
+		}
+		if i+1-l == k { // 已经收集满足了
+			return
+		}
+		qSort(nums, res, i+1, r, k-i-1+l, curResIdx)
+	}
+}
