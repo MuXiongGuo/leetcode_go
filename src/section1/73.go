03bbb7870a6ab9559070e9f0ab12080bdbc9ef52
gmx <1721030720@qq.com>
2023-09-04 21:35:01 +0800

    🍓 哈希记录行列, O(1)空间利用第0行 0列来当哈希表记录即可 之后再单独处理

---
+++ src/section1/73.go
@@ -0,0 +1,94 @@
+package main
+
+// 使用哈希标记 其实可以用[]int数组 int数组更节省空间
+func setZeroes(matrix [][]int) {
+	lineMap, rowMap := map[int]bool{}, map[int]bool{}
+	for i, row := range matrix {
+		for j, el := range row {
+			if el == 0 {
+				rowMap[i] = true
+				lineMap[j] = true
+			}
+		}
+	}
+
+	for i, row := range matrix {
+		for j, _ := range row {
+			if rowMap[i] || lineMap[j] {
+				matrix[i][j] = 0
+			}
+		}
+	}
+
+}
+
+// 官方 O(1)空间
+// 使用第0行和第0列来代替上面的两个数组
+func setZeroes(matrix [][]int) {
+	n, m := len(matrix), len(matrix[0])
+	row0, col0 := false, false
+	for _, v := range matrix[0] {
+		if v == 0 {
+			row0 = true
+			break
+		}
+	}
+	for _, r := range matrix {
+		if r[0] == 0 {
+			col0 = true
+			break
+		}
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = 0
+				matrix[0][j] = 0
+			}
+		}
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+		}
+	}
+	if row0 {
+		for j := 0; j < m; j++ {
+			matrix[0][j] = 0
+		}
+	}
+	if col0 {
+		for _, r := range matrix {
+			r[0] = 0
+		}
+	}
+}
+
+// 更有难度的 倒着看
+func setZeroes(matrix [][]int) {
+	n, m := len(matrix), len(matrix[0])
+	col0 := false
+	for _, r := range matrix {
+		if r[0] == 0 {
+			col0 = true
+		}
+		for j := 1; j < m; j++ {
+			if r[j] == 0 {
+				r[0] = 0
+				matrix[0][j] = 0
+			}
+		}
+	}
+	for i := n - 1; i >= 0; i-- {
+		for j := 1; j < m; j++ {
+			if matrix[i][0] == 0 || matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+		}
+		if col0 {
+			matrix[i][0] = 0
+		}
+	}
+}
