2da28e55dc6e51429a10bbf2ce0df10bd956cf28
gmx <1721030720@qq.com>
2023-09-06 09:33:35 +0800

    🥝 矩阵蛇形搜索 easy

--- src/section1/240.go
+++ src/section1/240.go
@@ -2,5 +2,15 @@ package main
 
 // 蛇形搜索
 func searchMatrix(matrix [][]int, target int) bool {
-
+	n, m := len(matrix), len(matrix[0])
+	for i, j := n-1, 0; i >= 0 && j < m; {
+		if matrix[i][j] == target {
+			return true
+		} else if matrix[i][j] < target {
+			j++
+		} else {
+			i--
+		}
+	}
+	return false
 }
